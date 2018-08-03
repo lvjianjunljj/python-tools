f5e93d4212778cfc9b5ab6d9b14d0307ed9e186b
jianjlv <ljj19921026@163.com>
2018-08-03 17:33:46 +0800

    fix bug

--- task/code_intelligence/run.py
+++ task/code_intelligence/run.py
@@ -10,7 +10,11 @@ import subprocess
 import task.code_intelligence.crawl_data as crawler
 import tools.batch_html_to_markdown as bh2m
 import tools.file_compress as file_compress
+import tools.file_size as file_size
 import tools.markdown_resolve as mr
+
+# ther version of every api doc
+index_version = 3
 
 encoding = r'utf-8'
 tf_python_dir = r'TensorFlow'
@@ -39,12 +43,25 @@ for index in path_list:
 
 # open(r'single_backslash.json', "w", encoding='utf-8').write(json.dumps(path_list).replace('\\\\','\\'))
 
-
 mr.batch_latex_to_embedded_html(root_md_dir)
 
+# get the root index.json file
+json_read = open(r'index.json', 'r', encoding='utf-8', errors='ignore')
+json_str = json_read.read()
+index_dict = eval(json_str)
+
 for simple_dir in [keras_dir, pytorch_dir, tf_python_dir]:
+    # generate index.json file
     shutil.copy(r'generate_index.js', os.path.join(root_md_dir, simple_dir, r'generate_index.js'))
     cmd = r'cd "' + os.path.join(root_md_dir, simple_dir) + r'" && node generate_index.js'
     subprocess.call(cmd, shell=True)
     os.remove(os.path.join(root_md_dir, simple_dir, r'generate_index.js'))
+
+    # correct the size and version attribute of root index.json file
+    index_dict[simple_dir]['size'] = file_size.get_dirs_size(os.path.join(root_md_dir, simple_dir))
+    index_dict[simple_dir]['version'] = index_version
+    # package the directory
     file_compress.make_targz(os.path.join(root_md_dir, simple_dir.lower() + r'.tar.gz'), os.path.join(root_md_dir, simple_dir))
+
+# save the change of root index.json file
+open(r'index.json', "w", encoding='utf-8').write(json.dumps(index_dict))
